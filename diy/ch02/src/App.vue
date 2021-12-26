3ebc6384c1f491b43cf67622d94681704ca554d8
SafeWinter <zandong_19@aliyun.com>
2021-12-27 00:42:23 +0800

    Added DIY files (learning notes and demo PWA project) for Chapter 2

---
+++ diy/ch02/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Github App</h1>
+    <GitHubTokenForm />
+    <DemoUser />
+    <DemoRepos />
+  </div>
+</template>
+
+<script>
+import GitHubTokenForm from "./components/GitHubTokenForm.vue";
+import DemoRepos from "./components/Repos.vue";
+import DemoUser from "./components/User.vue";
+
+export default {
+  name: "App",
+  components: {
+    GitHubTokenForm,
+    DemoRepos,
+    DemoUser,
+  },
+};
+</script>
